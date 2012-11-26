949cab0b8db7480876d3fd8852601211bc508930
Topogigi <luigi.valdemarin@gmail.com>
2012-11-26 19:37:17 +0100

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 48 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#48 SMP PREEMPT Mon Nov 26 16:56:33 CET 2012"
+#define LINUX_COMPILE_TIME "16:56:33"
+#define LINUX_COMPILE_BY "anddev"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.2 20110908 (prerelease) (Linaro GCC 4.6-2011.09-1) "
